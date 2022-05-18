a0a32b4be150265cf884055d42743d0beb98a399
duldul34 <huff022514@naver.com>
2022-05-19 05:01:51 +0900

    modify strlen

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -12,12 +12,12 @@
 
 #include "libft.h"
 
-int	ft_strlen(const char *s)
+size_t	ft_strlen(const char *str)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
